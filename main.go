24283e9093f243f129ca613a8c75fe679a8b91b7
twsnmp <ymi@twise.co.jp>
2021-01-04 07:14:04 +0900

    コマンド引数か環境変数でパラメータを取得する処理追加

--- main.go
+++ main.go
@@ -1,14 +1,34 @@
 package main
 
 import (
+	"flag"
 	"log"
 	"net/http"
+	"os"
+	"strings"
 
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
 	"github.com/rakyll/statik/fs"
 	_ "github.com/twsnmp/twsnmpfc/statik"
 )
+
+// From ENV or Command Args
+var dbPath string
+var logLevel string
+var masterPassword string
+
+func init() {
+	flag.StringVar(&dbPath, "dbpath", "./twsnmpfc.twdb", "Path to DB file")
+	flag.StringVar(&logLevel, "loglevel", "info", "Log Level")
+	flag.StringVar(&masterPassword, "password", "twsnmpfc!", "Master Password")
+	flag.VisitAll(func(f *flag.Flag) {
+		if s := os.Getenv("TWSNMPFC_" + strings.ToUpper(f.Name)); s != "" {
+			f.Value.Set(s)
+		}
+	})
+	flag.Parse()
+}
 
 func main() {
 	statikFS, err := fs.New()
