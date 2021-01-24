05c169cb602328c8103059ab01eb117e75206aec
twsnmp <ymi@twise.co.jp>
2021-01-25 06:54:27 +0900

    マップ表示のためにp5.jsを組み込んだ

--- spa/pages/map.vue
+++ spa/pages/map.vue
@@ -1,25 +1,55 @@
 <template>
-  <v-row>
-    <v-col class="text-center">
-      <logo />
-      <blockquote class="blockquote">
-        &#8220;ここにマップを表示する予定！！&#8221;
-        <footer>
-          <small>
-            <em>&mdash;TWSNMP FC</em>
-          </small>
-        </footer>
-      </blockquote>
-    </v-col>
-  </v-row>
+  <v-card class="mx-auto">
+    <div id="map"></div>
+    <blockquote class="blockquote">
+      &#8220;ここにマップを表示する予定！！&#8221;
+      <footer>
+        <small>
+          <em>&mdash;TWSNMP FC</em>
+        </small>
+      </footer>
+    </blockquote>
+  </v-card>
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
+import P5 from 'p5'
 
 export default {
-  components: {
-    Logo,
+  mounted() {
+    const script = function (p5) {
+      let speed = 2
+      let posX = 0
+      p5.setup = (_) => {
+        p5.createCanvas(2500, 5000)
+        p5.ellipse(p5.width / 2, p5.height / 2, 500, 500)
+      }
+
+      p5.draw = (_) => {
+        p5.background(0)
+        const degree = p5.frameCount * 3
+        const y = p5.sin(p5.radians(degree)) * 50
+        p5.push()
+        p5.translate(0, p5.height / 2)
+        p5.ellipse(posX, y, 50, 50)
+        p5.pop()
+
+        posX += speed
+        if (posX > p5.width || posX < 0) {
+          speed *= -1
+        }
+      }
+    }
+
+    new P5(script, 'map') // eslint-disable-line no-new
   },
 }
 </script>
+
+<style>
+#map {
+  width: 100%;
+  height: 800px;
+  overflow: scroll;
+}
+</style>
