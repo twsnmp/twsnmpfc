26c641214eb9cf4bba3902dd4e70e8f3bf01ed47
twsnmp <ymi@twise.co.jp>
2021-01-27 05:48:03 +0900

    マップ表示でノードの複数選択と移動を改善、ノードの削除などに対応

--- spa/plugins/map.js
+++ spa/plugins/map.js
@@ -2,6 +2,9 @@ import P5 from 'p5'
 
 const MAP_SIZE_X = 2500
 const MAP_SIZE_Y = 5000
+let mapRedraw = true
+
+let mapCallBack
 
 let nodes = {
   1: { Name: 'test', X: 100, Y: 100, Icon: 'desktop', State: 'high', ID: 1 },
@@ -11,16 +14,26 @@ let lines = [{ NodeID1: 1, NodeID2: 2, State1: 'high', State2: 'repair' }]
 
 const iconCodeMap = {}
 
+/* eslint prettier/prettier: 0 */
 const setIconCodeMap = (list) => {
   list.forEach((e) => {
     iconCodeMap[e.value] = String.fromCodePoint(e.code)
   })
-  iconCodeMap.unknown = String.fromCodePoint(0x1111)
+  iconCodeMap.unknown = String.fromCodePoint(0xF0A39)
 }
 
-const setMapData = (md) => {
-  nodes = {}
-  lines = []
+const setNodesData = (n) => {
+  nodes = n
+  mapRedraw = true
+}
+
+const setLinesData = (l) => {
+  lines = l
+  mapRedraw = true
+}
+
+const setMapCallBack = (cb) => {
+  mapCallBack = cb
 }
 
 const getIconCode = (icon) => {
@@ -39,17 +52,24 @@ const getStateColor = (state) => {
   return stateColorMap[state] ? stateColorMap[state] : 'gray'
 }
 
-const mapMain = function (p5) {
+const mapMain = (p5) => {
+  let startMouseX
+  let startMouseY
   let lastMouseX
   let lastMouseY
+  let dragMode  = 0 // 0 : None , 1: Select , 2 :Move
   const draggedNodes = []
   const selectedNodes = []
 
-  p5.setup = (_) => {
+  p5.setup = () => {
     p5.createCanvas(MAP_SIZE_X, MAP_SIZE_Y)
   }
 
-  p5.draw = (_) => {
+  p5.draw = () => {
+    if (! mapRedraw){
+      return
+    }
+    mapRedraw = false
     p5.background(250)
     // if(backimg){
     //   p5.image(backimg,0,0);
@@ -65,7 +85,7 @@ const mapMain = function (p5) {
       const xm = (x1 + x2) / 2
       const ym = (y1 + y2) / 2
       p5.push()
-      p5.strokeWeight(2)
+      p5.strokeWeight(1)
       p5.stroke(getStateColor(lines[k].State1))
       p5.line(x1, y1, xm, ym)
       p5.stroke(getStateColor(lines[k].State2))
@@ -88,17 +108,163 @@ const mapMain = function (p5) {
       p5.textFont('Material Design Icons')
       p5.textSize(32)
       p5.textAlign(p5.CENTER, p5.CENTER)
-      p5.fill(0)
-      p5.text(icon, 0, 0)
       p5.fill(getStateColor(nodes[k].State))
-      p5.text(icon, -1, -1)
+      p5.text(icon, 0, 0)
       p5.textFont('Arial')
       p5.textSize(12)
       p5.fill(0)
       p5.text(nodes[k].Name, 0, 32)
       p5.pop()
     }
+    if (dragMode === 1) {
+      let x = startMouseX
+      let y = startMouseY
+      let w = lastMouseX  - startMouseX
+      let h = lastMouseY  - startMouseY
+      if (startMouseX > lastMouseX){
+        x = lastMouseX
+        w = startMouseX - lastMouseX
+      }
+      if (startMouseY > lastMouseY){
+        y = lastMouseY
+        h = startMouseY - lastMouseY
+      }
+      p5.push()
+      p5.fill('rgba(250,250,250,0.6)')
+      p5.stroke(0)
+      p5.rect(x,y,w,h)
+      p5.pop()
+    } 
   }
+
+  p5.mouseDragged = () => {
+    if (
+      p5.winMouseX < 32 ||
+      p5.winMouseY < 32 ||
+      p5.winMouseY > p5.windowHeight * 0.75
+    ) {
+      return true
+    }
+    if (dragMode === 0) {
+      if (selectedNodes.length > 0 ){
+        dragMode = 2
+      } else {
+        dragMode = 1
+      }
+    }
+    if (dragMode === 1) {
+      dragSelectNodes()
+    } else if (dragMode === 2 && lastMouseX) {
+      dragMoveNodes()
+    }
+    lastMouseX = p5.mouseX
+    lastMouseY = p5.mouseY
+    return true
+  }
+
+  p5.mousePressed = () => {
+    if (
+      p5.keyIsDown(p5.SHIFT) &&
+      selectedNodes.length === 1 &&
+      setSelectNode(true)
+    ) {
+      editLine()
+      selectedNodes.length = 0
+      return true
+    } else if (setSelectNode(false)) {
+      mapRedraw = true
+    }
+    lastMouseX = p5.mouseX
+    lastMouseY = p5.mouseY
+    startMouseX = p5.mouseX
+    startMouseY = p5.mouseY
+    dragMode = 0
+    return true
+  }
+
+  p5.mouseClicked = () => {
+    return false
+  }
+
+  p5.mouseReleased = () => {
+    if( dragMode === 0) {
+      return false
+    }
+    if( dragMode === 1) {
+      dragMode = 0
+      mapRedraw = true
+      return false
+    }
+    if (draggedNodes.length === 0) {
+      return false
+    }
+    updateNodesPos()
+  }
+
+  p5.keyReleased = () => {
+    if (!p5.focused) {
+      return false
+    }
+    if (p5.keyCode === p5.DELETE || p5.keyCode === p5.BACKSPACE) {
+      // Delete
+      deleteNodes()
+    }
+    if (p5.keyCode === p5.ENTER) {
+      p5.doubleClicked()
+    }
+    return true
+  }
+
+  p5.doubleClicked = () => {
+    // Show Node Info
+    showNode()
+    return true
+  }
+
+  const dragMoveNodes = () => {
+    selectedNodes.forEach((id) => {
+      if (nodes[id]) {
+        nodes[id].X += p5.mouseX - lastMouseX
+        nodes[id].Y += p5.mouseY - lastMouseY
+        if (nodes[id].X < 16) {
+          nodes[id].X = 16
+        }
+        if (nodes[id].Y < 16) {
+          nodes[id].Y = 16
+        }
+        if (nodes[id].X > MAP_SIZE_X) {
+          nodes[id].X = MAP_SIZE_X - 16
+        }
+        if (nodes[id].Y > MAP_SIZE_Y) {
+          nodes[id].Y = MAP_SIZE_Y - 16
+        }
+        if (!draggedNodes.includes(id)) {
+          draggedNodes.push(id)
+        }
+      }
+    })
+    mapRedraw = true
+  }
+
+  const dragSelectNodes = () => {
+    selectedNodes.length = 0
+    const sx = startMouseX < lastMouseX ? startMouseX : lastMouseX
+    const sy = startMouseY < lastMouseY ? startMouseY : lastMouseY
+    const lx = startMouseX > lastMouseX ? startMouseX : lastMouseX
+    const ly = startMouseY > lastMouseY ? startMouseY : lastMouseY
+    for (const k in nodes) {
+      if (
+        nodes[k].X > sx &&
+        nodes[k].X < lx &&
+        nodes[k].Y > sy &&
+        nodes[k].Y < ly
+      ) {
+        selectedNodes.push(nodes[k].ID)
+      }
+    }
+    mapRedraw = true
+  }
+
   const setSelectNode = (bMulti) => {
     const l = selectedNodes.length
     for (const k in nodes) {
@@ -123,96 +289,69 @@ const mapMain = function (p5) {
     }
     return l !== selectedNodes.length
   }
-  p5.mouseDragged = () => {
-    if (
-      p5.winMouseX < 32 ||
-      p5.winMouseY < 32 ||
-      p5.winMouseY > p5.windowHeight * 0.75
-    ) {
-      return true
-    }
-    if (lastMouseX) {
-      selectedNodes.forEach((id) => {
-        if (nodes[id]) {
-          nodes[id].X += p5.mouseX - lastMouseX
-          nodes[id].Y += p5.mouseY - lastMouseY
-          if (nodes[id].X < 16) {
-            nodes[id].X = 16
-          }
-          if (nodes[id].Y < 16) {
-            nodes[id].Y = 16
-          }
-          if (nodes[id].X > MAP_SIZE_X) {
-            nodes[id].X = MAP_SIZE_X - 16
-          }
-          if (nodes[id].Y > MAP_SIZE_Y) {
-            nodes[id].Y = MAP_SIZE_Y - 16
-          }
-          if (!draggedNodes.includes(id)) {
-            draggedNodes.push(id)
-          }
-        }
+  // ノードを削除する
+  const deleteNodes = () => {
+    if (mapCallBack){
+      mapCallBack({
+        Cmd: 'deleteNodes',
+        Param: selectedNodes,
       })
-      p5.redraw()
     }
-    lastMouseX = p5.mouseX
-    lastMouseY = p5.mouseY
-    return true
-  }
-  p5.mousePressed = () => {
-    if (
-      p5.keyIsDown(p5.SHIFT) &&
-      selectedNodes.length > 0 &&
-      setSelectNode(true)
-    ) {
-      // createEditLinePane(selectedNodes[0], selectedNodes[1]);
-      selectedNodes.length = 0
-      return true
-    } else if (p5.keyIsDown(p5.CONTROL)) {
-      if (setSelectNode(true)) {
-        p5.redraw()
-      }
-    } else {
-      setSelectNode(false)
-    }
-    if (p5.mouseButton === p5.RIGHT && selectedNodes.length <= 1) {
-      const id = selectedNodes[0]
+    console.log('deleteNodes')
+    selectedNodes.forEach((id) => {
       if (nodes[id]) {
-      }
-    }
-    lastMouseX = p5.mouseX
-    lastMouseY = p5.mouseY
-    return true
-  }
-  p5.mouseClicked = () => {
-    return false
-  }
-  p5.mouseReleased = () => {
-    if (draggedNodes.length === 0) {
-      return
-    }
-    draggedNodes.forEach((id) => {
-      if (nodes[id]) {
-        // ノードの位置を保存
+        delete nodes[id]
       }
     })
+    mapRedraw = true
+  }
+  // Nodeの位置を保存する
+  const updateNodesPos = () => {
+    const list  = []
+    draggedNodes.forEach((id) => {
+      if (nodes[id]) {
+        // 位置を保存するノード
+        list.push({
+          ID: id,
+          X: nodes[id].X,
+          Y: nodes[id].Y,
+        })
+      }
+    })
+    if (mapCallBack) {
+      mapCallBack({
+        Cmd: 'updateNodesPos',
+        Param: list,
+      })
+    }
+    console.log('updateNodePos',list)
     draggedNodes.length = 0
   }
-  p5.keyReleased = () => {
-    if (!p5.focused) {
-      return false
+  const showNode = () => {
+    if (selectedNodes.length !== 1 ){
+      return
     }
-    if (p5.keyCode === p5.DELETE || p5.keyCode === p5.BACKSPACE) {
-      // Delete
+    console.log('showNode')
+    if (mapCallBack) {
+      mapCallBack({
+        Cmd: 'showNode',
+        Param: selectedNodes[0],
+      })
     }
-    if (p5.keyCode === p5.ENTER) {
-      p5.doubleClicked()
-    }
-    return true
   }
-  p5.doubleClicked = () => {
-    // Show Node Info
-    return true
+  const editLine = () => {
+    if (selectedNodes.length !== 2 ){
+      return
+    }
+    console.log("editLine")
+    if (mapCallBack) {
+      mapCallBack({
+        Cmd: 'editLine',
+        Param: selectedNodes,
+      })
+    }
+    selectedNodes.length = 0
+    mapRedraw = true
   }
 }
 
@@ -224,5 +363,7 @@ export default (context, inject) => {
   inject('showMAP', showMAP)
   inject('setIconCodeMap', setIconCodeMap)
   inject('setStateColorMap', setStateColorMap)
-  inject('setMapData', setMapData)
+  inject('setNodesData', setNodesData)
+  inject('setLinesData', setLinesData)
+  inject('setMapCallBack', setMapCallBack)
 }
