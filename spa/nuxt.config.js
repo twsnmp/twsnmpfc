eb8db048c26bcbb0ba07cefbe0903a2666dc56ea
twsnmp <ymi@twise.co.jp>
2022-01-29 06:34:21 +0900

    robotoフォントを追加

--- spa/nuxt.config.js
+++ spa/nuxt.config.js
@@ -21,7 +21,10 @@ export default {
   },
 
   // Global CSS (https://go.nuxtjs.dev/config-css)
-  css: ['~/node_modules/@mdi/font/css/materialdesignicons.css'],
+  css: [
+    '~/node_modules/@mdi/font/css/materialdesignicons.css',
+    '~/node_modules/@fontsource/roboto/index.css',
+  ],
 
   // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
   plugins: [
