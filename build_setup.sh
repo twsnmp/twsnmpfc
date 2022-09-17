1d2b479264f9f7b9ebb3904dd7dd2bacf405fed4
twsnmp <ymi@twise.co.jp>
2022-09-17 16:42:11 +0900

    ビルド環境構築のコマンドを修正

--- build_setup.sh
+++ build_setup.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 apt update
-apt -y nodejs npm python libx11-dev libxkbfile-dev libxkbfile-dev  xserver-xorg-dev libxi-dev libxext-dev
+apt install -y nodejs npm python libx11-dev libxkbfile-dev libxkbfile-dev  xserver-xorg-dev libxi-dev libxext-dev
 go get github.com/rakyll/statik
