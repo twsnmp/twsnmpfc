e188346d778dd39ca78ca420cb41ea864ef29450
twsnmp <ymi@twise.co.jp>
2023-08-10 16:14:21 +0900

    JWTの更新によりログインできなくなる問題の修正

--- webapi/login.go
+++ webapi/login.go
@@ -6,7 +6,7 @@ import (
 	"net/http"
 	"time"
 
-	"github.com/golang-jwt/jwt"
+	"github.com/golang-jwt/jwt/v5"
 	"github.com/labstack/echo/v4"
 	"github.com/twsnmp/twsnmpfc/datastore"
 	"github.com/twsnmp/twsnmpfc/security"
