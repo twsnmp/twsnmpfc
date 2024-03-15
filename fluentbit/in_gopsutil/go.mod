4d56baf884395d1343f4ff14224b08db3d8e3510
twsnmp <ymi@twise.co.jp>
2024-03-15 17:04:49 +0900

    fluentbitのgopsutilプラグインを追加

---
+++ fluentbit/in_gopsutil/go.mod
@@ -0,0 +1,20 @@
+module github.com/twsnmp/twsnmpfc/fluentbit/in_gopsutil
+
+go 1.22.0
+
+require (
+	github.com/fluent/fluent-bit-go v0.0.0-20230731091245-a7a013e2473c
+	github.com/shirou/gopsutil/v3 v3.24.2
+)
+
+require (
+	github.com/go-ole/go-ole v1.2.6 // indirect
+	github.com/lufia/plan9stats v0.0.0-20211012122336-39d0f177ccd0 // indirect
+	github.com/power-devops/perfstat v0.0.0-20210106213030-5aafc221ea8c // indirect
+	github.com/shoenig/go-m1cpu v0.1.6 // indirect
+	github.com/tklauser/go-sysconf v0.3.12 // indirect
+	github.com/tklauser/numcpus v0.6.1 // indirect
+	github.com/ugorji/go/codec v1.1.7 // indirect
+	github.com/yusufpapurcu/wmi v1.2.4 // indirect
+	golang.org/x/sys v0.18.0 // indirect
+)
---
+++ fluentbit/in_gopsutil/in_gopsutil.go
@@ -0,0 +1,136 @@
+package main
+
+/*
+#include <stdlib.h>
+*/
+import "C"
+import (
+	"fmt"
+	"log"
+	"time"
+	"unsafe"
+
+	"github.com/fluent/fluent-bit-go/input"
+	"github.com/shirou/gopsutil/v3/cpu"
+	"github.com/shirou/gopsutil/v3/disk"
+	"github.com/shirou/gopsutil/v3/host"
+	"github.com/shirou/gopsutil/v3/load"
+	"github.com/shirou/gopsutil/v3/mem"
+)
+
+var collection = ""
+var diskPath = "/"
+
+//export FLBPluginRegister
+func FLBPluginRegister(def unsafe.Pointer) int {
+	log.Printf("[in_gopsutil] Register called")
+	return input.FLBPluginRegister(def, "gopsutil", "Input plugin for gopsutil")
+}
+
+// (fluentbit will call this)
+// plugin (context) pointer to fluentbit context (state/ c code)
+//
+//export FLBPluginInit
+func FLBPluginInit(plugin unsafe.Pointer) int {
+	collection = input.FLBPluginConfigKey(plugin, "collection")
+	diskPath = input.FLBPluginConfigKey(plugin, "disk_path")
+	if diskPath == "" {
+		diskPath = "/"
+	}
+
+	log.Printf("[in_gopsutil] collection= '%s'", collection)
+	return input.FLB_OK
+}
+
+//export FLBPluginInputCallback
+func FLBPluginInputCallback(data *unsafe.Pointer, size *C.size_t) int {
+	flb_time := input.FLBTime{time.Now()}
+	entry := []interface{}{flb_time}
+	switch collection {
+	case "cpu.percent.percpu":
+		entry = append(entry, getCPUPercent(true))
+	case "cpu.percent":
+		entry = append(entry, getCPUPercent(false))
+	case "cpu.times":
+		if d, err := cpu.Times(false); err == nil && len(d) == 1 {
+			entry = append(entry, d[0])
+		}
+	case "mem.vm":
+		if d, err := mem.VirtualMemory(); err == nil {
+			entry = append(entry, d)
+		}
+	case "mem.swap":
+		if d, err := mem.SwapMemory(); err == nil {
+			entry = append(entry, d)
+		}
+	case "host.info":
+		if d, err := host.Info(); err == nil {
+			entry = append(entry, d)
+		}
+	case "host.temp":
+		entry = append(entry, getSensorTemp())
+	case "load.avg":
+		if d, err := load.Avg(); err == nil {
+			entry = append(entry, d)
+		}
+	case "disk.usage":
+		if d, err := disk.Usage(diskPath); err == nil {
+			entry = append(entry, d)
+		}
+	default:
+		log.Println("unknown collection")
+		return input.FLB_ERROR
+	}
+	enc := input.NewEncoder()
+	packed, err := enc.Encode(entry)
+	if err != nil {
+		log.Printf("[in_gopsutil] encode err:%v", err)
+		return input.FLB_ERROR
+	}
+
+	length := len(packed)
+	*data = C.CBytes(packed)
+	*size = C.size_t(length)
+	return input.FLB_OK
+}
+
+//export FLBPluginInputCleanupCallback
+func FLBPluginInputCleanupCallback(data unsafe.Pointer) int {
+	return input.FLB_OK
+}
+
+//export FLBPluginExit
+func FLBPluginExit() int {
+	log.Printf("[in_gopsutil] Exit called")
+	return input.FLB_OK
+}
+
+func getCPUPercent(percpu bool) map[string]interface{} {
+	ret := make(map[string]interface{})
+	if cpus, err := cpu.Percent(0, percpu); err == nil {
+		if percpu {
+			for i, v := range cpus {
+				ret[fmt.Sprintf("cpu%d", i+1)] = v
+			}
+		} else if len(cpus) > 0 {
+			ret["cpu"] = cpus[0]
+		}
+	}
+	return ret
+}
+
+func getSensorTemp() map[string]interface{} {
+	ret := make(map[string]interface{})
+	if stl, err := host.SensorsTemperatures(); err == nil {
+		for _, st := range stl {
+			if st.Temperature != 0 {
+				ret[st.SensorKey] = st.Temperature
+			}
+		}
+	}
+	return ret
+
+}
+
+func main() {
+}
