ebf2106b905fe24dab655364f1f08c255466bb94
twsnmp <ymi@twise.co.jp>
2024-07-31 17:09:00 +0900

    バージョンの更新

--- logger/sshd.go
+++ logger/sshd.go
@@ -33,7 +33,7 @@ func sshd(stopCh chan bool) {
 	}
 	sv := ssh.Server{
 		Addr:             fmt.Sprintf(":%d", SshdPort),
-		Version:          "TWSNNMP FC v1.40.0",
+		Version:          "TWSNNMP FC v1.41.0",
 		HostSigners:      []ssh.Signer{signer},
 		IdleTimeout:      time.Second * 30,
 		MaxTimeout:       time.Minute * 10,
